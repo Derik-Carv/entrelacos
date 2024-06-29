04c7be4a7d0a88170628a32977f4a0f467578d3d
Derik-Carv <derikcarv.dc@gmail.com>
2024-06-29 02:50:05 -0300

    correção de css para melhor responsvidade e adicionado o script de cor do produto por troca de imagem

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
-function topborboleta(imageId, newSrc) {
-    document.getElementById(imageId).src = newSrc;
+const btn = document.querySelectorAll('div button');
+for (let i=0; i < btn.length; i++) {
+    btn[i].addEventListener(`click`, cores)
+}
+function cores() {
+    this.parentNode.parentNode.querySelector('img').src = 'src/pictures/'+this.name+'-'+this.id+'.jpg'
 }
