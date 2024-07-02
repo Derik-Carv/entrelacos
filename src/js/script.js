659b1bdf53f2131cd993696ddc36bae9270a0f71
Derik-Carv <derikcarv.dc@gmail.com>
2024-07-02 20:53:34 -0300

    adiçào de carrinho de compras, estilização e renomeação de atributos

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,14 @@
 const btn = document.querySelectorAll('div button');
-const carrinho = document.querySelector('#addcar')
+const addcar = document.querySelector('#addcar')
+const opencar = document.querySelector('.carrinho')
 for (let i=0; i < btn.length; i++) {
     btn[i].addEventListener(`click`, cores)
 }
 function cores() {
-    if (this.name != carrinho.name) {
+    if (this.name != addcar.name) {
         this.parentNode.parentNode.querySelector('img').src = 'src/pictures/'+this.name+'-'+this.id+'.jpg'    
     } else {
-        return alert('Item adicionado ao carrinho.')
-    }  
+        opencar.style.display = 'block'
+        return alert('Item adicionado ao carrinho.');
+    }
 }
